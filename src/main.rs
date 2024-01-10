1ee24f9a7f4262e9f81ef9cabb723067892e156e
Qi Wang <mpiglet@outlook.com>
2024-01-10 08:49:17 +0800

    tmp commit

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ static HELLO: &[u8] = b"Hello, World!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
-    for (i, byte) in HELLO.iter().enumerate() {
+    for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(2 * i as isize) = byte;
             *vga_buffer.offset(2 * i as isize + 1) = 0xb;
